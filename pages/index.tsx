5c93e3ce71db0d9e40093dd687082e471f4dce4c
Fahad Qaseem <fahad@Fahads-MacBook-Pro-2.local>
2025-08-04 02:34:50 -0400

    Fix: escape apostrophe to pass JSX build

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,8 @@ export default function Home() {
       <main className="min-h-screen flex flex-col items-center justify-center bg-white text-gray-800 px-4">
         
         <h1 className="text-5xl font-bold mb-4">Hi, I&#39;m Fahad Qaseem</h1>
-        <p>... I help brands craft engaging content that connects and converts.</p>
-
-
-
-
-
-        
+        <p>I help brands craft engaging content that connects and converts.</p>
+        <p></p>
         <a
           href="mailto:fahad@example.com"
           className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
