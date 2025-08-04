b46ff561e52cd1e37c5e73b850d54ccc07a4545d
Fahad Qaseem <fahad@Fahads-MacBook-Pro-2.local>
2025-08-04 02:39:34 -0400

    Your update message here

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,10 @@ export default function Home() {
         
         <h1 className="text-5xl font-bold mb-4">Hi, I&#39;m Fahad Qaseem</h1>
         <p>I help brands craft engaging content that connects and converts.</p>
-        <p></p>
+        
         <a
           href="mailto:fahad@example.com"
-          className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+          className="mt-4 bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
         >
           Contact Me
         </a>
